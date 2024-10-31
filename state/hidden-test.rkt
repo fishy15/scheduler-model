5bc9a0b1c585bfe9633ad1be8203d3ad70ee76c0
= <38575855+fishy15@users.noreply.github.com>
2024-10-31 02:46:39 -0500

    Add hidden state construction from sample

---
+++ state/hidden-test.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require rackunit
+         json
+         "hidden.rkt")
+
+(define single-datapoint
+  (with-input-from-file "single.json"
+    (lambda ()
+      (list-ref (read-json) 0))))
+
+(displayln (read-from-json single-datapoint))
+
---
+++ state/reader.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(require json)
+
+(with-input-from-file "data.json"
+  (lambda ()
+    (let* ([data (read-json)]
+           [entry (list-ref data 0)]
+           [cpu (hash-ref entry 'cpu)])
+      (displayln cpu))))
