b9983a9cc42390f20eb6030c8c11f5637d88c042
aprakas <aprakas@node.aprakas-232629.wisr-pg0.wisc.cloudlab.us>
2024-11-22 08:01:12 +0000

    Add a basic validator

--- state/checker.rkt
+++ state/checker.rkt
@@ -5,8 +5,23 @@
 
 ;; Checks if the given hidden state could produce the given visible state
 (define (valid hidden visible)
-  #t)
+  (only-move-from-nonidle hidden visible))
 
+(define (only-move-from-nonidle hidden visible)
+  ;; Currently only nr-tasks is defined for the visible state,
+  ;; so we need to find for which CPUs we know the number of tasks
+  ;; and force it to be equal.
+  ;; We don't have this information currently,
+  ;; instead we will check if we move a task from a CPU, it has >0 tasks.
+  (define (check-sd-buf sd-buf)
+    (define env (sd-entry-lb-logmsg sd-buf))
+    (if env
+        (begin
+          (define src-cpu (lb-env-src-cpu env))
+          (> (hidden-cpu-nr-tasks (get-cpu-by-id visible src-cpu)) 0))
+        #t))
+  (andmap check-sd-buf (visible-state-sd-buf visible)))
+  
 ;; Checks if the load balancer made the correct decision
 (define (correct hidden visible)
   (overloaded-to-idle hidden visible))
