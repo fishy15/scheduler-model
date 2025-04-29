5fa72f799c25f1e00290a3b16cd0bfdf68e5dec8
= <38575855+fishy15@users.noreply.github.com>
2025-04-29 15:27:55 -0500

    Add variants of each invariant that only run if should-we-balance is set

--- src/checker/invariants.rkt
+++ src/checker/invariants.rkt
@@ -40,7 +40,7 @@
 |#
 
 ;; If there are more tasks cores, then we want distribute tasks
-(define (right-to-work hidden visible)
+(define (right-to-work hidden visible [ignore-swb #t])
   (define (check-sd sd-info)
     (let* ([visible-sd (visible-sd-info-sd sd-info)]
            [src-cpu (visible-sd-src-cpu visible-sd)]
@@ -48,6 +48,10 @@
            [tasks-moved
             (match (visible-sd-nr-tasks-moved visible-sd)
               ['null 0]
+              [x x])]
+           [should-we-balance
+            (match (visible-sd-should-we-balance visible-sd)
+              ['null #f]
               [x x])]
            [hidden-cpus (hidden-state-cpus hidden)]
            [hidden-old-has-tasks (lambda (cpu) (> (hidden-cpu-nr-tasks cpu) 0))]
@@ -60,19 +64,27 @@
                   [dst-cpu (> (+ nr-tasks tasks-moved) 0)]
                   [_ (> tasks-moved 0)])))]
            [new-cpus-with-tasks (length (filter hidden-new-has-tasks hidden-cpus))])
-      (<= old-cpus-with-tasks new-cpus-with-tasks)))
+      ;; run the check if we ignore-swb value or should-we-balance is set
+      (implies (or ignore-swb should-we-balance)
+               (<= old-cpus-with-tasks new-cpus-with-tasks))))
   (andmap check-sd (visible-state-per-sd-info visible)))
 
 ;; We should not move a task from src -> dst if there is some other
 ;; core with 2x the util (constant can be adjusted as needed)
 ;; If no move happens, then return true.
 (define MOVE-RATIO 2)
-(define (moves-from-busiest hidden visible)
+(define (moves-from-busiest hidden visible [ignore-swb #t])
   (define (check-sd sd-info)
     (let* ([visible-sd (visible-sd-info-sd sd-info)]
            [src-cpu (visible-sd-src-cpu visible-sd)]
-           [dst-cpu (visible-sd-dst-cpu visible-sd)])
+           [dst-cpu (visible-sd-dst-cpu visible-sd)]
+           [should-we-balance
+            (match (visible-sd-should-we-balance visible-sd)
+              ['null #f]
+              [x x])])
       (cond
+        ;; if ignore-swb is false and we didn't run should we balance, then ignore
+        [(and (not ignore-swb) (not should-we-balance)) #t]
         [(not (or (eq? src-cpu 'null) (eq? dst-cpu 'null)))
          (let* ([hidden-src-cpu (hidden-get-cpu-by-id hidden src-cpu)]
                 [src-util (hidden-cpu-cpu-util hidden-src-cpu)]
@@ -86,7 +98,13 @@
 ;; then the scheduler attempts to make progress by moving some tasks
 ;; from an overloaded CPU to dst.
 (define (create-overloaded-to-idle-check this-hidden-overloaded?)
-  (define (overloaded-to-idle hidden visible)
+  (define (overloaded-to-idle hidden visible [ignore-swb #t])
+    (define (swb-is-true sd-info)
+      (let* ([sd (visible-sd-info-sd sd-info)]
+             [should-we-balance (visible-sd-should-we-balance sd)])
+        (or ignore-swb should-we-balance)))
+    (define sd-infos-with-swb
+      (filter swb-is-true (visible-state-per-sd-info visible)))
     (define (moves-to-idle sd-info)
       (let* ([visible-sd (visible-sd-info-sd sd-info)]
              [dst-cpu (visible-sd-dst-cpu visible-sd)])
@@ -109,8 +127,8 @@
              (and src-overloaded dst-idle))]
           [else #f])))
     (let ([any-overloaded (hidden-any-cpus-overloaded? hidden)]
-          [to-idle (ormap moves-to-idle (visible-state-per-sd-info visible))]
-          [idle-to-overloaded (ormap moves-idle-to-overloaded (visible-state-per-sd-info visible))])
+          [to-idle (ormap moves-to-idle sd-infos-with-swb)]
+          [idle-to-overloaded (ormap moves-idle-to-overloaded sd-infos-with-swb)])
       (implies (and any-overloaded to-idle)
                idle-to-overloaded)))
   overloaded-to-idle)
@@ -134,6 +152,10 @@
 ;; Don't use moves-from-busiest --- the relationship between load and tasks is difficult to entangle
 (define invariants
   (list (invariant "right-to-work" right-to-work)
+        (invariant "right-to-work-swb" (lambda (h v) (right-to-work h v #f)))
         (invariant "moves-from-busiest" moves-from-busiest)
+        (invariant "moves-from-busiest-swb" (lambda (h v) (moves-from-busiest h v #f)))
         (invariant "overloaded-to-idle" overloaded-to-idle)
-        (invariant "overloaded-to-idle-cfs" overloaded-to-idle-cfs)))
+        (invariant "overloaded-to-idle-swb" (lambda (h v) (overloaded-to-idle h v #f)))
+        (invariant "overloaded-to-idle-cfs" overloaded-to-idle-cfs)
+        (invariant "overloaded-to-idle-cfs-swb" (lambda (h v) (overloaded-to-idle-cfs h v #f)))))
